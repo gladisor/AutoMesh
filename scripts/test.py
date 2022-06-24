444ac521aafd478169145589f233c93eecc1b659
Tristan Shah <tristanshah@Tristans-MacBook-Air.local>
2022-06-24 09:41:46 +0200

    update

--- scripts/test.py
+++ scripts/test.py
@@ -3,7 +3,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import automesh
+from automesh import data
 import open3d as o3d
 
 pcd = o3d.io.read_point_cloud("/Users/tristanshah/Desktop/1000shapes/AutoMesh/data/GRIPS22/DE-UKU-02-0001_LeftAtrium.ply")
