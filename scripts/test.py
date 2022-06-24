e9e0c7882a14c921ce4b4051606e39b22b0db655
Tristan Shah <tristanshah@Tristans-MacBook-Air.local>
2022-06-24 09:38:43 +0200

    able to load point cloud

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,10 @@
-from pytorch3d.io import IO
+## Allows us to access the automesh library from outside
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-mesh = IO().load_mesh('data/GRIPS22/DE-UKU-02-0001_LeftAtrium.ply')
+import automesh
+import open3d as o3d
+
+pcd = o3d.io.read_point_cloud("/Users/tristanshah/Desktop/1000shapes/AutoMesh/data/GRIPS22/DE-UKU-02-0001_LeftAtrium.ply")
+print(pcd)
