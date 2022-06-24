2b8a4b2634b4573332ff761bd350a1618f976c6b
Tristan Shah <tristanshah@Tristans-MacBook-Air.local>
2022-06-24 10:07:40 +0200

    starter code

--- scripts/test.py
+++ scripts/test.py
@@ -1,10 +1,21 @@
-## Allows us to access the automesh library from outside
+## allows us to access the automesh library from outside
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from automesh import data
+## third party
 import open3d as o3d
+import numpy as np
 
-pcd = o3d.io.read_point_cloud("/Users/tristanshah/Desktop/1000shapes/AutoMesh/data/GRIPS22/DE-UKU-02-0001_LeftAtrium.ply")
-print(pcd)
+## local source
+import automesh
+
+data = o3d.io.read_triangle_mesh("/Users/tristanshah/Desktop/1000shapes/AutoMesh/data/GRIPS22/DE-UKU-02-0001_LeftAtrium.ply")
+print(data)
+
+
+# points = np.array(pcd.points)
+# normals = np.array(pcd.normals)
+# covariances = np.array(pcd.covariances)
+
+# print(covariances)
