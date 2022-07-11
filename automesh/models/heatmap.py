63accfd1e21dfc4243140a3be9eb5173f55f3a12
Tristan Shah <tristanshah@Tristans-MacBook-Air.local>
2022-07-11 14:01:50 +0200

    adding comments

--- automesh/models/heatmap.py
+++ automesh/models/heatmap.py
@@ -8,6 +8,9 @@ from torch_geometric.data import Data, Batch
 from pytorch_lightning import LightningModule
 
 class HeatMapRegressor(LightningModule):
+    """
+    Predicts landmarks on a mesh by generating heatmap distributions per channel.
+    """
     def __init__(
             self,
             base: nn.Module,
@@ -18,6 +21,7 @@ class HeatMapRegressor(LightningModule):
             **kwargs) -> None:
         super().__init__()
 
+        ## constructing graph neural network
         self.base = base(**kwargs)
         self.opt = opt
         self.opt_kwargs = opt_kwargs
@@ -32,27 +36,18 @@ class HeatMapRegressor(LightningModule):
 
     @staticmethod
     def normalized_mean_error(pred_points: torch.tensor, true_points: torch.tensor) -> torch.tensor:
+        ## mean euclidan distance between points
         return (true_points - pred_points).norm(dim = 1).mean()
     
     @staticmethod
     def evaluate_heatmap(heatmap: torch.tensor, data: Data) -> torch.tensor:
+        ## get points with highest predicted probability
         pred_points = HeatMapRegressor.predict_points(heatmap, data.x)
+        ## extract true highest probability points from ground truth heatmap
         true_points = HeatMapRegressor.predict_points(data.y, data.x)
+        ## computing average distance between points
         return HeatMapRegressor.normalized_mean_error(pred_points, true_points)
 
-    # def evaluate_model(self, batch: Batch, batch_idx: int):
-    #     with torch.no_grad():
-    #         distance = 0.0
-            
-    #         # evaluate each graph in batch seperately
-    #         for i in range(batch.num_graphs):
-    #             data = batch.get_example(i)
-    #             distance += HeatMapRegressor.evaluate_heatmap(self(data), data)
-
-    #         distance /= batch.num_graphs
-
-    #         val_loss = self.landmark_loss(self(batch), batch.y)
-    
     def forward(self, x: Union[Data, Batch]) -> torch.tensor:
         if x.edge_attr != None:
             return self.base(x.pos, x.edge_index, x.edge_attr)
@@ -60,7 +55,6 @@ class HeatMapRegressor(LightningModule):
             return self.base(x.pos, x.edge_index)
     
     def configure_optimizers(self):
-        # opt = self.optimizer(self.base.parameters(), self.lr)
         return self.opt(self.base.parameters(), **self.opt_kwargs)
 
     def landmark_loss(self, y_hat, y) -> torch.tensor:
@@ -74,6 +68,7 @@ class HeatMapRegressor(LightningModule):
     def training_step(self, batch: Batch, batch_idx) -> torch.tensor:
         ## compute landmark loss on each channel
         loss = self.landmark_loss(self(batch), batch.y)
+
         self.log(
             'train_loss', loss, 
             batch_size = batch.num_graphs, 
@@ -81,17 +76,21 @@ class HeatMapRegressor(LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
+        ## we dont want to differentiate
         with torch.no_grad():
             distance = 0.0
+
+            ## calculate evaluation metric independently on each graph
             for i in range(batch.num_graphs):
                 data = batch.get_example(i)
                 distance += HeatMapRegressor.evaluate_heatmap(self(data), data)
 
+            ## average over number of graphs
             distance /= batch.num_graphs
 
+            ## compute loss on validation batch as well
             val_loss = self.landmark_loss(self(batch), batch.y)
         
         self.log(
             'val_performance', {'val_loss': val_loss, 'distance': distance}, 
-            batch_size = batch.num_graphs, 
-            )
+            batch_size = batch.num_graphs)
