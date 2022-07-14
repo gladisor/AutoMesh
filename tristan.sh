2650d8e8b3346b73b70153dd64fd83c66bf4bce7
Shah <tshah@grips06.zib.de>
2022-07-14 16:34:59 +0200

    neptune

--- tristan.sh
+++ tristan.sh
@@ -6,9 +6,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64G
 #SBATCH --job-name automesh
-#SBATCH --output=log2.out
+#SBATCH --output=log.out
 
-export WANDB_API_KEY = "9a6992594ce0851dbccb151860b2751420a558a3"
-export WANDB_ENTITY= "tshah"
 source /scratch/htc/tshah/miniconda3/bin/activate automesh2
 srun python /scratch/htc/tshah/AutoMesh/scripts/main.py
