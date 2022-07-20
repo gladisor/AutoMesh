ce1284cdfdb8178d83710b80387b8786f73deeb0
Zimmer <pzimmer@grips04.zib.de>
2022-07-20 12:32:44 +0200

    compute configuration testing

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pickle
+import time
 # import socketserver
 # socketserver.TCPServer.allow_reuse_address = True
 
@@ -11,22 +12,47 @@ import torch
 import torch.nn as nn
 import torch_geometric.transforms as T
 from torch_geometric.data import LightningDataset
-from torch_geometric.nn import GraphNorm, SAGEConv, GATConv
+from torch_geometric.nn import GraphNorm, SAGEConv, GATConv, FeaStConv, GCNConv
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.plugins import DDPSpawnPlugin, environments
 from pytorch_lightning.loggers import CSVLogger
+from pytorch_lightning.callbacks import Callback
 from optuna import Trial, create_study, create_trial
 from optuna.trial import FixedTrial
 import pandas as pd
 
 ## local source
-from automesh.models.architectures import ParamGCN
+from automesh.models.architectures import ParamGCN, ParamGraphUNet
 from automesh.data.data import LeftAtriumHeatMapData
 from automesh.models.heatmap import HeatMapRegressor
 from automesh.loss import (
     AdaptiveWingLoss, DiceLoss, BCEDiceLoss, 
     JaccardLoss, FocalLoss, TverskyLoss, FocalTverskyLoss)
 from automesh.data.transforms import preprocess_pipeline, rotation_pipeline
+
+
+
+class timerCallback(Callback):
+    def __init__(self):
+        self.startingtime=time.time()
+        # self.traininginit=time.time()
+        print ('init callback', self.startingtime)
+        
+    def on_init_start(self,trainer):
+        self.startingtime =time.time()
+        print('starting',self.startingtime)
+    
+    # def on_init_end(self,trainer):
+    #     print('init endingXXXXXXXXXXXXXXXXXXX')
+    
+    def on_train_start(self, trainer, pl_module):
+        #self.traininginit = time.time()-self.startingtime
+        print("Trainingtime init= ", time.time()-self.startingtime)
+    
+    def on_train_end(self, trainer, pl_module):
+        
+        print("Trainingtime all = ",time.time()-self.startingtime)
+        
 
 def heatmap_regressor(trial: Trial):
     seed_everything(42)
@@ -44,22 +70,30 @@ def heatmap_regressor(trial: Trial):
         train_dataset = train,
         val_dataset = val,
         batch_size = batch_size,
-        num_workers = 4,
-        persistent_workers = True
+        num_workers = 10,
+        #pin_memory=False,
+        #persistent_workers = False,
 	)
-
+    
+    
     hidden_channels = trial.suggest_int('hidden_channels', 32, 256)
     num_layers = trial.suggest_int('num_layers', 3, 8)
     lr = trial.suggest_float('lr', 0.00001, 0.001)
+    
+    
+    
 
     params = {
         'base': ParamGCN,
         'base_kwargs': {
-            'conv_layer': SAGEConv,
+            'conv_layer': GCNConv,
             'conv_kwargs': {},
+            #'pool_layer': TopKPooling,
+            #'pool_kwargs': {'ratio':0.5},
             'in_channels': 3,
             'hidden_channels': hidden_channels,
             'num_layers': num_layers,
+            #'depth': num_layers,
             'out_channels': 8,
             'act': nn.GELU,
             'act_kwargs': {},
@@ -81,8 +115,8 @@ def heatmap_regressor(trial: Trial):
 
     logger = CSVLogger(save_dir = 'results', name = 'optuna')
 
-    num_nodes = 1
-    devices = -1
+   # num_nodes = 1
+    devices = 4
 
     ddp_spawn_plugin = DDPSpawnPlugin(
 #        num_nodes = num_nodes,
@@ -90,11 +124,12 @@ def heatmap_regressor(trial: Trial):
         find_unused_parameters = False) 
 
     trainer = Trainer(
-#        num_nodes = num_nodes,
+        callbacks=[timerCallback()],
+#       num_nodes = num_nodes,
         accelerator = 'gpu',
         strategy = ddp_spawn_plugin,
         devices = devices,
-        max_epochs = 50,
+        max_epochs = 150,
         logger = logger,
         )
 
@@ -105,11 +140,14 @@ def heatmap_regressor(trial: Trial):
     return history['val_nme'].min()
 
 if __name__ == '__main__':
+    # global startingtime
+    # startingtime=0.0
+    
     study = create_study()
     study.optimize(heatmap_regressor, n_trials = 100)
 
     with open('study.pkl', 'wb') as f:
         pickle.dump(study, f)
 
-    #trial = FixedTrial({'hidden_channels': 832, 'num_layers': 8, 'lr': 0.0007})
+    #trial = FixedTrial({'hidden_channels': 200, 'num_layers': 8, 'lr': 0.0007})
     #heatmap_regressor(trial)
