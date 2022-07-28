b38243c228cd3ad7582be92cd742adcff0eb5896
Tristan Shah <tristanshah@Tristans-MacBook-Air.local>
2022-07-28 09:27:18 +0200

    changing bash script

--- main.sh
+++ main.sh
@@ -4,15 +4,15 @@
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
-#SBATCH --time=23:59:59
-#SBATCH --job-name automesh_db
-#SBATCH --output=singleTrial.out
-#SBATCH --mail-user=paul.zimmer@zib.de
+#SBATCH --time=06:00:00
+#SBATCH --job-name am_edge
+#SBATCH --output=log.out
+#SBATCH --mail-user=tristan.shah@sjsu.edu
 
 export CUDA_LAUNCH_BLOCKING=1
 
-source /scratch/htc/pzimmer/miniconda3/bin/activate automesh2
-srun python /scratch/htc/pzimmer/AutoMesh/scripts/main.py
+source /scratch/htc/tshah/miniconda3/bin/activate automesh2
+srun python /scratch/htc/tshah/AutoMesh/scripts/main.py
 
 
 ### salloc -p gpu --time 12:00:00 --nodes 1 --gpus-per-node 4 --gres gpu:4 --cpus-per-task 4 --job-name automesh2
